3ee596d94226a452c1e769aa3b3a0f04d42e2c32
Amit Mungare <amungare27@gmail.com>
2021-03-27 17:21:54 +0530

    print ith node in linked list

---
+++ coding Niinjas DSA/src/linkedList1/PrintIthNode.java
@@ -0,0 +1,50 @@
+package linkedList1;
+
+import java.util.Scanner;
+
+public class PrintIthNode {
+
+	public static Node<Integer> input() {
+		Node<Integer> head = null, tail=null;
+		Scanner s=new Scanner(System.in);
+		int data = s.nextInt();
+		 
+		while(data != -1) {
+			Node<Integer> newNode = new Node<Integer>(data);
+			if(head == null) {
+				head = newNode;
+				tail = newNode;
+			}else {
+				tail.next=newNode;
+				tail = newNode;
+			}
+			data=s.nextInt();
+		}
+		return head;
+	}
+
+
+	public static void ithNode(Node<Integer> head, int d) {
+		
+		int count=0;
+
+		while(head != null) {
+			count++;
+			head =head.next;
+			if(count ==d) {
+				System.out.println();
+				System.out.println(head.data);
+			}
+		}
+		
+	}
+	
+	
+	public static void main(String[] args) {		
+		Node<Integer> head = input();
+		int d=3;
+		ithNode(head,d);
+	}
+
+	
+}
