4e3850fa1fb2453ee0a9f18e2cd86b85b5f78cf0
Amit Mungare <amungare27@gmail.com>
2021-03-29 21:02:19 +0530

    reverse LL

---
+++ coding Niinjas DSA/src/linkedList2/ReverseLL.java
@@ -0,0 +1,62 @@
+package linkedList2;
+
+import java.util.Scanner;
+
+
+public class ReverseLL {
+	
+	public static Node<Integer> rev(Node<Integer> head){
+		
+		if(head == null || head.next ==null) {
+			return head;
+		}
+		
+		Node<Integer> smallans =rev(head.next);
+		Node<Integer> temp = smallans;
+		while(temp.next != null){
+			temp = temp.next;
+		}
+		temp.next=head;
+		head.next =null;
+		return smallans;
+		
+	}
+	
+
+		
+		public static Node<Integer> input() {
+			Node<Integer> head = null, tail=null;
+			Scanner s=new Scanner(System.in);
+			int data = s.nextInt();
+			 
+			while(data != -1) {
+				Node<Integer> newNode = new Node<Integer>(data);
+				if(head == null) {
+					head = newNode;
+					tail = newNode;
+				}else {
+					tail.next=newNode;
+					tail = newNode;
+				}
+				data=s.nextInt();
+			}
+			return head;
+		}
+		
+			
+		public static void print(Node<Integer> head) {		
+			while(head != null) {
+				System.out.print(head.data+" ");
+				head = head.next;
+			}		
+		}
+		
+		
+		public static void main(String[] args) {		
+			Node<Integer> head = input();
+			head = rev(head);
+			print(head);
+		}
+
+
+}
