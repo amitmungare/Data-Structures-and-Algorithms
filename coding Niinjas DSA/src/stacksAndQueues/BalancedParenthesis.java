efb33cf7881dd9ca62942dfb1d86731665f08a0b
Amit Mungare <amungare27@gmail.com>
2021-04-02 16:05:29 +0530

    isBalanced

---
+++ coding Niinjas DSA/src/stacksAndQueues/BalancedParenthesis.java
@@ -0,0 +1,46 @@
+package stacksAndQueues;
+
+public class BalancedParenthesis {
+	
+	 public static boolean isBalanced(String exp) {
+		 
+		 StackUsingLL<Character> s = new StackUsingLL<>();
+			
+			int i=0;
+			while(i<exp.length()) {
+				
+				if(exp.charAt(i)=='(') {					
+					s.push(exp.charAt(i));
+				}
+				if(exp.charAt(i)==')') {
+					if(s.isEmpty()) {
+						return false;
+					}
+					
+					if(s.top()=='(') {
+						s.pop();						
+					}else {
+						return false;
+					}
+					
+				}			
+			i++;	
+			}
+			if(!s.isEmpty()) {
+				return false;
+			}
+			return true;
+	    }
+	 
+	 
+
+	public static void main(String[] args) {		
+
+		
+	String str = "((())";
+	boolean t =isBalanced(str);
+	System.out.println(t);
+		
+	}
+
+}
