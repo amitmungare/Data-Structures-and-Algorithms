b4fc318c1ba640d21d3b04048c59e9f1105d68e8
Amit Mungare <amungare27@gmail.com>
2021-04-02 12:41:03 +0530

    stack use class

---
+++ coding Niinjas DSA/src/stacksAndQueues/StackUse.java
@@ -0,0 +1,19 @@
+package stacksAndQueues;
+
+public class StackUse {
+
+	public static void main(String[] args) {
+	
+		StackUsingArray s =new StackUsingArray(3);
+		
+		for(int i=1; i<=11; i++) {
+			s.push(i);
+		}
+		
+		while(!s.isEmpty()) {
+			System.out.print(s.pop()+" ");
+		}
+
+	}
+
+}
