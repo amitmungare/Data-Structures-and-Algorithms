e6d8d770eafd392087990d83adbd3edfa0bb8c53
Amit Mungare <amungare27@gmail.com>
2021-04-03 12:06:41 +0530

    Reverse Queue

---
+++ coding Niinjas DSA/src/stacksAndQueues/ReverseQueue.java
@@ -0,0 +1,22 @@
+package stacksAndQueues;
+
+import java.util.Queue;
+import java.util.Stack;
+
+public class ReverseQueue {
+
+	
+	public static void reverseQueue(Queue<Integer> input) {
+		if(input.size()<=1) {
+			return;
+		}
+		int temp =input.poll();
+		reverseQueue(input);
+		input.add(temp);
+	}
+	
+
+	public static void main(String[] args) {
+	
+	}  
+}
