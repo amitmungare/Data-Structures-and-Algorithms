d62ea4f652eb6033d0f55cc75c36966765838e56
Amit Mungare <amungare27@gmail.com>
2021-04-03 12:06:11 +0530

    Reverse a Stack

---
+++ coding Niinjas DSA/src/stacksAndQueues/ReverseStack.java
@@ -0,0 +1,36 @@
+package stacksAndQueues;
+
+import java.util.Stack;
+
+public class ReverseStack{
+
+	public static void reverseStack(Stack<Integer> input, Stack<Integer> extra) {
+
+		if(input.size()<=1) {
+			return;
+		}
+	
+		int lastele = input.pop();
+	
+		reverseStack(input, extra);
+	
+		while(!input.isEmpty()) {
+			int top = input.pop();
+			extra.push(top);
+		}
+	
+		input.push(lastele);
+	
+		while(!extra.empty()){
+			int top=extra.pop();
+			input.push(top);
+		}
+	
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
