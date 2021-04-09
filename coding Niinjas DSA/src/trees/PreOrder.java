44dda725ba4cfb1fe637fba53c47006c2ae014f6
Amit Mungare <amungare27@gmail.com>
2021-04-09 17:57:33 +0530

    pre order

---
+++ coding Niinjas DSA/src/trees/PreOrder.java
@@ -0,0 +1,40 @@
+package trees;
+
+public class PreOrder {
+	
+	public static void preOrder(TreeNode<Integer> root) {
+		if(root==null) {
+			return;
+		}
+		
+		System.out.print(root.data+" ");
+		for(int i=0;i<root.children.size();i++) {
+			preOrder(root.children.get(i));
+		}
+	}
+
+	
+	public static void main(String[] args) {		
+		TreeNode<Integer> root= input();
+		preOrder(root);
+	}
+	
+	private static TreeNode<Integer> input() {
+		TreeNode<Integer> root0 =new TreeNode<Integer>(1);
+		TreeNode<Integer> root1 =new TreeNode<Integer>(2);
+		TreeNode<Integer> root2 =new TreeNode<Integer>(3);
+		TreeNode<Integer> root3 =new TreeNode<Integer>(4);
+		TreeNode<Integer> root4 =new TreeNode<Integer>(5);
+		TreeNode<Integer> root5 =new TreeNode<Integer>(6);
+		TreeNode<Integer> root6 =new TreeNode<Integer>(7);
+		root0.children.add(root1);
+		root0.children.add(root2);
+		root0.children.add(root3);
+		root1.children.add(root4);
+		root1.children.add(root5);
+		root2.children.add(root6);
+		return root0;
+	}
+
+	
+}
