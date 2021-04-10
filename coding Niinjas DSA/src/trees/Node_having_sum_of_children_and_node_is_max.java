3d3056853518d3feecaa9695ef92cd57e7e4ebc0
Amit Mungare <amungare27@gmail.com>
2021-04-10 15:53:48 +0530

    Node having sum of children and node is max

---
+++ coding Niinjas DSA/src/trees/Node_having_sum_of_children_and_node_is_max.java
@@ -0,0 +1,75 @@
+package trees;
+
+import stacksAndQueues.QueueUsingLL;
+
+public class Node_having_sum_of_children_and_node_is_max {
+
+	public static TreeNode<Integer> Sum(TreeNode<Integer> root){
+		
+		if(root==null) {
+			return root;
+		}
+		if(root.children.size()==0) {
+			return root;
+		}
+		
+		int sum=root.data;
+		for(int i=0 ; i<root.children.size(); i++) {
+			sum += root.children.get(i).data;
+		}
+		TreeNode<Integer> ans=root;
+		for(int i=0; i<root.children.size(); i++) {
+			TreeNode<Integer> child = Sum(root.children.get(i));
+			int childsum = child.data;
+			for(int j=0 ; j<child.children.size();j++) {
+				childsum+=child.children.get(j).data;
+			}
+			if(childsum>sum) {
+				ans=child;
+			}
+		}
+		return ans;
+	}
+
+	
+	public static void main(String[] args) {		
+		TreeNode<Integer> root= input();
+		System.out.println(Sum(root).data);
+	}
+	
+	private static TreeNode<Integer> input() {
+		TreeNode<Integer> root0 =new TreeNode<Integer>(1);
+		TreeNode<Integer> root1 =new TreeNode<Integer>(2);
+		TreeNode<Integer> root2 =new TreeNode<Integer>(3);
+		TreeNode<Integer> root3 =new TreeNode<Integer>(4);
+		TreeNode<Integer> root4 =new TreeNode<Integer>(5);
+		TreeNode<Integer> root5 =new TreeNode<Integer>(6);
+		TreeNode<Integer> root6 =new TreeNode<Integer>(7);
+		root0.children.add(root1);
+		root0.children.add(root2);
+		root0.children.add(root3);
+		root1.children.add(root4);
+		root1.children.add(root5);
+		root2.children.add(root6);
+		return root0;
+	}
+
+	public static void printLW(TreeNode<Integer> root) {
+		
+		QueueUsingLL<TreeNode<Integer>> queue= new QueueUsingLL<>();
+		queue.enqueue(root);
+		
+		while(!queue.isEmpty()) {
+			
+			for(int i=queue.size(); i!=0; i--) {
+				TreeNode<Integer> frontNode =queue.dequeue();
+				System.out.print(frontNode.data+" ");
+				for(int j=0; j<frontNode.children.size(); j++) {
+					queue.enqueue(frontNode.children.get(j));
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
