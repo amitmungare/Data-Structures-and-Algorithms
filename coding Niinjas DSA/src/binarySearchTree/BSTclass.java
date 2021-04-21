400e7fee649ae142354ee645c030c8c79c0425da
Amit Mungare <amungare27@gmail.com>
2021-04-21 16:13:15 +0530

    BST class insert delete and update

--- coding Niinjas DSA/src/binarySearchTree/BSTclass.java
+++ coding Niinjas DSA/src/binarySearchTree/BSTclass.java
@@ -1,19 +1,67 @@
 package binarySearchTree;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+
 import binaryTree.BinaryTreeNode;
 
 public class BSTclass {
 	
 	BinaryTreeNode<Integer> root;
 	
-	public void insertData(int data) {
-		
+	public void insert(int data) {
+		root=insert(root, data);
 	}
 	
+	private BinaryTreeNode<Integer> insert(BinaryTreeNode<Integer> root, int data) {
+		if(root==null) {
+			BinaryTreeNode<Integer> ans = new BinaryTreeNode<Integer>(data);
+			return ans;
+		}
+		if(data > root.data) {
+			root.right = insert(root.right, data);
+		}else {
+			root.left=insert(root.left, data);
+		}
+		return root;
+	}
+
 	public void deleteData(int data) {
-		
+		root=remove(root, data);
 	}
 	
+	private BinaryTreeNode<Integer> remove(BinaryTreeNode<Integer> root, int data) {
+		
+		if(root==null) {
+			return null;
+		}
+		if(root.data > data) {
+			root.left =remove(root.left, data);
+			return root;
+		}else if(root.data < data) {
+			root.right = remove(root.right, data);
+			return root;
+		}else {
+			if(root.left==null && root.left ==null) {
+				return null;
+			}
+			else if(root.left ==null) {
+				return root.right;
+			}
+			else if(root.right == null) {
+				return root.left;
+			}else {
+				BinaryTreeNode<Integer> min =root.right;
+				while(min.left != null) {
+					min=min.left;
+				}
+				root.data =min.data;
+				root.right = remove(root.right, min.data);
+				return root;
+			} 
+		}
+		
+	}
+
 	public boolean hasData(int data) {
 		return hasDataHelper(data, root);
 	}
