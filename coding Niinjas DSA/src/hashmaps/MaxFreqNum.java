d75f5b2e05d9123590b1d83e0e3dc406d55a5024
Amit Mungare <amungare27@gmail.com>
2021-04-26 23:28:49 +0530

    max freq number

---
+++ coding Niinjas DSA/src/hashmaps/MaxFreqNum.java
@@ -0,0 +1,40 @@
+package hashmaps;
+
+import java.util.HashMap;
+
+public class MaxFreqNum {
+	
+	 public static int maxFreqNum(int[] a){ 
+		
+		 HashMap<Integer, Integer> map = new HashMap<>();
+		 int max=0;
+		 int num=a[0];
+		 		 
+		 for(int i=0;i<a.length;i++) {
+			 if(map.containsKey(a[i])) {				 
+				 map.put(a[i],map.get(a[i])+1);				 
+			 }else {
+				 map.put(a[i], 1);
+			 }
+		 }
+		 
+		 for(int i=0;i<a.length;i++) {
+			 
+			 if(map.get(a[i])> max) {
+				 max = map.get(a[i]);
+				 num = a[i];
+			 }
+			 
+		 }
+		 return num;
+	 }
+	
+
+	public static void main(String[] args) {
+		
+		int a[] = {1,2,2,1};
+		System.out.println(maxFreqNum(a));
+
+	}
+
+}
