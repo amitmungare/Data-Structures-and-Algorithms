705a4f2a0f53f6069f59e78b20986b75aaf9c36f
Amit Mungare <amungare27@gmail.com>
2021-04-29 23:16:01 +0530

    lca of BST

---
+++ coding Niinjas DSA/src/binarySearchTree/LCAofBST.java
@@ -0,0 +1,39 @@
+package binarySearchTree;
+
+import binaryTree.BinaryTreeNode;
+
+public class LCAofBST {
+
+	static int getLCA(BinaryTreeNode<Integer> root, int a, int b) {
+
+		if(root==null) {
+			return -1;
+		}
+		
+		if(root.data==a ||root.data==b) {
+			return root.data;
+		}
+		
+		if(root.data<a && root.data<b) {
+			return getLCA(root.right, a, b);
+		}
+		if(root.data>a && root.data>b) {
+			return getLCA(root.left, a, b);
+		}
+		
+		int left = getLCA(root.left, a, b);
+		int right = getLCA(root.right, a, b);
+		
+		if(left==-1 && right==-1) {
+			return -1;
+		}else if(left==-1) {
+			return right;
+		}else if(right==-1) {
+			return left;
+		}else {
+			return root.data;
+		}
+		
+	}
+	
+}
