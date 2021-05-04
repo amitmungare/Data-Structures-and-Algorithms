3f38c1fc2af5cb96ca61357abb6344cf3d5c1415
Amit Mungare <amungare27@gmail.com>
2021-05-04 22:42:56 +0530

    stair case

---
+++ coding Niinjas DSA/src/dp1/Staircase.java
@@ -0,0 +1,23 @@
+package dp1;
+
+public class Staircase {
+	
+	public static long staircase(int n) {
+        
+        long s[] = new long[n+2];
+        s[0]=1;
+        s[1]=1;
+        s[2]=2;
+        
+        for(int i=3;i<=n;i++) {
+        	s[i]=s[i-1]+s[i-2]+s[i-3];
+        }
+        return s[n];
+	}
+
+	public static void main(String[] args) {
+		System.out.println(staircase(4));
+
+	}
+
+}
