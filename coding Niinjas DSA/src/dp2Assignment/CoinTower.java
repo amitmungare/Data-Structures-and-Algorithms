ace1801b790978b8e4298f233dbf335259d021ab
Amit Mungare <amungare27@gmail.com>
2021-05-08 23:54:12 +0530

    coin tower

---
+++ coding Niinjas DSA/src/dp2Assignment/CoinTower.java
@@ -0,0 +1,27 @@
+package dp2Assignment;
+
+public class CoinTower {
+	
+//	public static String findWinner(int n, int x, int y) {
+//		//Your code goes here
+//	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+
+
+
+//Sample Input 1 :
+//4 2 3
+//Sample Output 1 :
+//Whis
+//Sample Input 2 :
+//10 2 4
+//Sample Output 2 :
+//Beerus
