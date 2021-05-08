daa5fe7874a0b836951354f3bef2fef3cd33176e
Amit Mungare <amungare27@gmail.com>
2021-05-08 23:53:53 +0530

    coin tower

--- coding Niinjas DSA/src/dp2Assignment/Allpossibleways.java
+++ coding Niinjas DSA/src/dp2Assignment/Allpossibleways.java
@@ -1,6 +1,12 @@
 package dp2Assignment;
 
 public class Allpossibleways {
+	
+
+//    public static int allWays(int x, int n) {
+//    	
+//    }
+        
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -8,3 +14,15 @@ public class Allpossibleways {
 	}
 
 }
+
+
+
+
+//Sample Input 1 :
+//10 2
+//Sample Output 1 :
+//1
+//Sample Input 2 :
+//100 2
+//Sample Output 2 :
+//3
