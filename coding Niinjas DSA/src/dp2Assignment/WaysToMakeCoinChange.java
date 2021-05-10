3c295c7ff584c36468e7e651665bf4966b59b775
Amit Mungare <amungare27@gmail.com>
2021-05-10 22:51:22 +0530

    ways to make coin change

---
+++ coding Niinjas DSA/src/dp2Assignment/WaysToMakeCoinChange.java
@@ -0,0 +1,28 @@
+package dp2Assignment;
+
+public class WaysToMakeCoinChange {
+	
+	
+//	public static int countWaysToMakeChange(int denominations[], int value){
+//        // Write your code here
+//	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+
+//Sample Output 1 :
+//4
+//Explanation to Sample Input 1 :
+//Number of ways are - 4 total i.e. (1,1,1,1), (1,1, 2), (1, 3) and (2, 2).
+//Sample Input 2 :
+//6
+//1 2 3 4 5 6
+//250
+//Sample Output 2 :
+//13868903
