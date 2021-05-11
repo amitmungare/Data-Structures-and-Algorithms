86caf35556adc78ca5b668159e12c8e75fd60887
Amit Mungare <amungare27@gmail.com>
2021-05-11 23:42:02 +0530

    shortest subsequence

---
+++ coding Niinjas DSA/src/dp2Assignment/ShortestSubsequence.java
@@ -0,0 +1,25 @@
+package dp2Assignment;
+
+public class ShortestSubsequence {
+	
+	
+//	public int solve(String S,String V){
+//		
+//	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+
+
+//Sample Input 1:
+//babab
+//babba
+//Sample Output 1:
+//3
