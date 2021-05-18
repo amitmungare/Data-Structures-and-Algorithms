de0567c65e297f873a71a239653a7ef586f677e6
Amit Mungare <amungare27@gmail.com>
2021-05-18 11:06:31 +0530

    balance parenthesis

--- coding Niinjas DSA/src/stacksAndQueues/BalancedParenthesis.java
+++ coding Niinjas DSA/src/stacksAndQueues/BalancedParenthesis.java
@@ -1,10 +1,41 @@
 package stacksAndQueues;
 
+import java.util.Stack;
+
 public class BalancedParenthesis {
 	
 	 public static boolean isBalanced(String exp) {
 		 
-		 StackUsingLL<Character> s = new StackUsingLL<>();
+//		 StackUsingLL<Character> s = new StackUsingLL<>();
+//			
+//			int i=0;
+//			while(i<exp.length()) {
+//				
+//				if(exp.charAt(i)=='(') {					
+//					s.push(exp.charAt(i));
+//				}
+//				if(exp.charAt(i)==')') {
+//					if(s.isEmpty()) {
+//						return false;
+//					}				
+//					if(s.top()=='(') {
+//						s.pop();						
+//					}else {
+//						return false;
+//					}					
+//				}			
+//			i++;	
+//			}
+//			if(!s.isEmpty()) {
+//				return false;
+//			}
+//			return true;
+		 
+		 
+		 
+		 
+		 
+		 Stack<Character> s = new Stack();
 			
 			int i=0;
 			while(i<exp.length()) {
@@ -15,8 +46,9 @@ public class BalancedParenthesis {
 				if(exp.charAt(i)==')') {
 					if(s.isEmpty()) {
 						return false;
-					}				
-					if(s.top()=='(') {
+					}
+                 char u = s.peek();
+                 if(u=='(') {
 						s.pop();						
 					}else {
 						return false;
@@ -28,15 +60,17 @@ public class BalancedParenthesis {
 				return false;
 			}
 			return true;
+		 
+		 
+		 
 	    }
 	 
-	 
+	
 
 	public static void main(String[] args) {				
 		String str = "((())";
 		boolean t =isBalanced(str);
 		System.out.println(t);
-		
 	}
 
 }
