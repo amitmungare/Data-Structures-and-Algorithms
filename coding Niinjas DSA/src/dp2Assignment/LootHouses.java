f7cb62d2abcd4ce27a6a269e6eaea26ed64e1988
Amit Mungare <amungare27@gmail.com>
2021-05-20 22:52:58 +0530

    loot houses

---
+++ coding Niinjas DSA/src/dp2Assignment/LootHouses.java
@@ -0,0 +1,28 @@
+package dp2Assignment;
+
+public class LootHouses {
+	
+//	public static int maxMoneyLooted(int[] houses) {
+//		//Your code goes here
+//	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+
+
+//Sample Input 1 :
+//6
+//5 5 10 100 10 5
+//Sample Output 1 :
+//110
+//Sample Input 2 :
+//6
+//10 2 30 20 3 50
+//Sample Output 2 :
+//90
