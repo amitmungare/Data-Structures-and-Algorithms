4ae9469fed2603b17e3ab033098b341f51ede28e
Amit Mungare <amungare27@gmail.com>
2021-05-20 22:52:34 +0530

    longest increasing subsequence

---
+++ coding Niinjas DSA/src/dp2Assignment/LongestIncreasingSubsequence.java
@@ -0,0 +1,30 @@
+package dp2Assignment;
+
+public class LongestIncreasingSubsequence {
+	
+//	  public static int lis(int arr[]) {
+//		  
+//	  }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+
+
+//Sample Input 1 :
+//6
+//5 4 11 1 16 8
+//Sample Output 1 :
+//3
+//Sample Output Explanation
+//Length of longest subsequence is 3 i.e. (5,11,16) or (4,11,16).
+//Sample Input 2 :
+//3
+//1 2 2
+//Sample Output 2 :
+//2
