93f3363cddbc10ad83ae9923dab06edea8087d9f
Amit Mungare <amungare27@gmail.com>
2021-05-24 00:51:54 +0530

    matrix chain multiplication

---
+++ coding Niinjas DSA/src/dp2Assignment/MatrixChainMultiplication.java
@@ -0,0 +1,24 @@
+package dp2Assignment;
+
+public class MatrixChainMultiplication {
+	
+//	public static int mcm(int[] p){
+//		
+//		
+//
+//		}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+//Sample Input 1:
+//3
+//10 15 20 25
+//Sample Output 1:
+//8000
