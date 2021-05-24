0d7ef566a674566b7297002b1498fad0f5e23d37
Amit Mungare <amungare27@gmail.com>
2021-05-25 00:06:58 +0530

    max square matrix

---
+++ coding Niinjas DSA/src/dp2Assignment/MaximumSquareMatrixWithAllZeros.java
@@ -0,0 +1,33 @@
+package dp2Assignment;
+
+public class MaximumSquareMatrixWithAllZeros {
+	
+	
+//	public static int findMaxSquareWithAllZeros(int[][] input){
+//		
+//	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+
+//Sample Input 1:
+//3 3
+//1 1 0
+//1 1 1
+//1 1 1
+//Sample Output 1:
+//1
+//Sample Input 2:
+//4 4
+//0 0 0 0
+//0 0 0 0
+//0 0 0 0
+//0 0 0 0
+//Sample Output 2:
+//4
