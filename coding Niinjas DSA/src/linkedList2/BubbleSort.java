f7b5439fb9fea7c52b9504711260a98abeb89947
Amit Mungare <amungare27@gmail.com>
2021-09-14 00:41:19 +0530

    bubble sort

--- coding Niinjas DSA/src/linkedList2/BubbleSort.java
+++ coding Niinjas DSA/src/linkedList2/BubbleSort.java
@@ -4,7 +4,17 @@ public class BubbleSort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+int ans =1234;
+int newans=0;
+        
+        while(ans !=0){
+            int r=ans%10;
+            newans = newans*10+r;
+            ans =ans/10;
+        }
+        
+        System.out.println(newans);
+		
 	}
 
 }
